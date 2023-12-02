7bcbb9aaea64dfc1528fc49cb28a4ef43620aec0
jacek13 <micjac2000@o2.pl>
2023-12-02 15:04:38 +0100

    fill repo

---
+++ backend/TicTacToe.domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace TicTacToe.domain
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ backend/TicTacToe.webapi/Controllers/HealthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TicTacToe.webapi.Controllers
+{
+
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        public HealthController() { }
+
+        [HttpGet("/health")]
+        public object GetInfo()
+        {
+            return new
+            {
+                EnvironmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"),
+                ServerDate = DateTime.Now,
+            };
+        }
+
+    }
+}
