d10fb7f01a728320021a94985117b175c34024ae
jacek13 <micjac2000@o2.pl>
2023-12-08 23:47:05 +0100

    unit tests fixed

--- backend/TicTacToe.test/UnitTestGameLogic.cs
+++ backend/TicTacToe.test/UnitTestGameLogic.cs
@@ -12,29 +12,31 @@ namespace TicTacToe.test
 
             game.AddUser(Guid.NewGuid().ToString());
             game.AddUser(Guid.NewGuid().ToString());
-            game.Users[0].Type = FieldType.Cross;
-            game.Users[1].Type = FieldType.Circle;
 
-            game.NewMatchState(1, 1, game.Users.First().Type);
-            Assert.Throws<DomainException>(() => game.NewMatchState(1, 1, game.Users.First().Type));
+            var (playerFirst, playerSecond) = game.AssignFieldsToPlayers();
+            var isPlayerOneBeginner = playerFirst.Type == game.WhichPlayerBegin();
+
+            game.NewMatchState(1, 1, playerFirst.Type);
+            Assert.Throws<DomainException>(() => game.NewMatchState(1, 1, playerFirst.Type));
         }
 
         [Fact(DisplayName = "User One Should Win")]
         public void GameUserOneShouldWin()
         {
             var game = new Game();
-            var userOne = (Guid.NewGuid(), game.TicTacToeMatch.State == MatchState.CircleTurn ? FieldType.Circle : FieldType.Cross);
-            var userTwo = (Guid.NewGuid(), userOne.Item2 == FieldType.Circle ? FieldType.Cross : FieldType.Circle);
 
-            game.AddUser(userOne.ToString());
-            game.AddUser(userTwo.ToString());
+            game.AddUser(Guid.NewGuid().ToString());
+            game.AddUser(Guid.NewGuid().ToString());
 
-            MatchState state = MatchState.Warmup;
-            state = game.NewMatchState(1, 1, userOne.Item2);
-            state = game.NewMatchState(0, 0, userTwo.Item2);
-            state = game.NewMatchState(1, 0, userOne.Item2);
-            state = game.NewMatchState(2, 0, userTwo.Item2);
-            state = game.NewMatchState(1, 2, userOne.Item2);
+            var (playerFirst, playerSecond) = game.AssignFieldsToPlayers();
+            var isPlayerOneBeginner = playerFirst.Type == game.WhichPlayerBegin();
+
+            MatchState state = game.TicTacToeMatch.State;
+            state = game.NewMatchState(1, 1, isPlayerOneBeginner ? playerFirst.Type : playerSecond.Type);
+            state = game.NewMatchState(0, 0, isPlayerOneBeginner ? playerSecond.Type : playerFirst.Type);
+            state = game.NewMatchState(1, 0, isPlayerOneBeginner ? playerFirst.Type : playerSecond.Type);
+            state = game.NewMatchState(2, 0, isPlayerOneBeginner ? playerSecond.Type : playerFirst.Type);
+            state = game.NewMatchState(1, 2, isPlayerOneBeginner ? playerFirst.Type : playerSecond.Type);
 
             Assert.True(state == MatchState.CircleWon || state == MatchState.CrossWon);
         }
@@ -46,40 +48,49 @@ namespace TicTacToe.test
 
             game.AddUser(Guid.NewGuid().ToString());
             game.AddUser(Guid.NewGuid().ToString());
-            game.Users[0].Type = FieldType.Cross;
-            game.Users[1].Type = FieldType.Circle;
+
+            var (playerFirst, playerSecond) = game.AssignFieldsToPlayers();
+            var isPlayerOneBeginner = playerFirst.Type == game.WhichPlayerBegin();
 
             MatchState state = MatchState.Warmup;
-            state = game.NewMatchState(1, 1, game.Users.First().Type);
-            state = game.NewMatchState(0, 0, game.Users.Last().Type);
-            Assert.Throws<DomainException>(() => game.NewMatchState(1, 1, game.Users.First().Type));
-            Assert.Throws<DomainException>(() => game.NewMatchState(1, 1, game.Users.First().Type));
-            state = game.NewMatchState(0, 1, game.Users.First().Type);
+            state = game.NewMatchState(1, 1, isPlayerOneBeginner ? playerFirst.Type : playerSecond.Type);
+            state = game.NewMatchState(0, 0, isPlayerOneBeginner ? playerSecond.Type : playerFirst.Type);
+            Assert.Throws<DomainException>(() => game.NewMatchState(1, 1, isPlayerOneBeginner ? playerFirst.Type : playerSecond.Type));
+            Assert.Throws<DomainException>(() => game.NewMatchState(1, 1, isPlayerOneBeginner ? playerFirst.Type : playerSecond.Type));
+            state = game.NewMatchState(1, 0, isPlayerOneBeginner ? playerFirst.Type : playerSecond.Type);
+            state = game.NewMatchState(0, 1, isPlayerOneBeginner ? playerSecond.Type : playerFirst.Type);
+            state = game.NewMatchState(1, 2, isPlayerOneBeginner ? playerFirst.Type : playerSecond.Type);
+            Assert.True(state == MatchState.CircleWon || state == MatchState.CrossWon);
         }
 
-        [Fact(DisplayName = "Playes could should be able to play new game after first one")]
+        [Fact(DisplayName = "Playes should be able to play new game after first one")]
         public void NewGameShouldCreate()
         {
             var game = new Game();
-            var userOne = (Guid.NewGuid(), game.TicTacToeMatch.State == MatchState.CircleTurn ? FieldType.Circle : FieldType.Cross);
-            var userTwo = (Guid.NewGuid(), userOne.Item2 == FieldType.Circle ? FieldType.Cross : FieldType.Circle);
 
-            game.AddUser(userOne.ToString());
-            game.AddUser(userTwo.ToString());
+            game.AddUser(Guid.NewGuid().ToString());
+            game.AddUser(Guid.NewGuid().ToString());
+
+            var (playerFirst, playerSecond) = game.AssignFieldsToPlayers();
+            var isPlayerOneBeginner = playerFirst.Type == game.WhichPlayerBegin();
 
             MatchState state = MatchState.Warmup;
-            state = game.NewMatchState(1, 1, userOne.Item2);
-            state = game.NewMatchState(0, 0, userTwo.Item2);
-            state = game.NewMatchState(1, 0, userOne.Item2);
-            state = game.NewMatchState(2, 0, userTwo.Item2);
-            state = game.NewMatchState(1, 2, userOne.Item2);
+            state = game.NewMatchState(1, 1, isPlayerOneBeginner ? playerFirst.Type : playerSecond.Type);
+            state = game.NewMatchState(0, 0, isPlayerOneBeginner ? playerSecond.Type : playerFirst.Type);
+            state = game.NewMatchState(1, 0, isPlayerOneBeginner ? playerFirst.Type : playerSecond.Type);
+            state = game.NewMatchState(2, 0, isPlayerOneBeginner ? playerSecond.Type : playerFirst.Type);
+            state = game.NewMatchState(1, 2, isPlayerOneBeginner ? playerFirst.Type : playerSecond.Type);
 
             game.NewGame();
-            state = game.NewMatchState(2, 1, userOne.Item2);
-            state = game.NewMatchState(0, 0, userTwo.Item2);
-            state = game.NewMatchState(2, 0, userOne.Item2);
-            state = game.NewMatchState(2, 0, userTwo.Item2);
-            state = game.NewMatchState(2, 2, userOne.Item2);
+
+            (playerFirst, playerSecond) = game.AssignFieldsToPlayers();
+            isPlayerOneBeginner = playerFirst.Type == game.WhichPlayerBegin();
+
+            state = game.NewMatchState(2, 1, isPlayerOneBeginner ? playerFirst.Type : playerSecond.Type);
+            state = game.NewMatchState(0, 0, isPlayerOneBeginner ? playerSecond.Type : playerFirst.Type);
+            state = game.NewMatchState(2, 0, isPlayerOneBeginner ? playerFirst.Type : playerSecond.Type);
+            state = game.NewMatchState(1, 0, isPlayerOneBeginner ? playerSecond.Type : playerFirst.Type);
+            state = game.NewMatchState(2, 2, isPlayerOneBeginner ? playerFirst.Type : playerSecond.Type);
 
             Assert.True(state == MatchState.CircleWon || state == MatchState.CrossWon);
         }
